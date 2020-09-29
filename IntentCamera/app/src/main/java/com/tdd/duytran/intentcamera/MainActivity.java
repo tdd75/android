87f2c019481c946ee03135bc78215402dfc712f6
Duy Tran <tranducduy7520@gmail.com>
2020-09-30 01:20:45 +0700

    Intent Camera

--- IntentCamera/app/src/main/java/com/tdd/duytran/intentcamera/MainActivity.java
+++ IntentCamera/app/src/main/java/com/tdd/duytran/intentcamera/MainActivity.java
@@ -1,14 +1,62 @@
 package com.tdd.duytran.intentcamera;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.util.BitSet;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView imgPicture;
+    Button btnCamera;
+    int REQUEST_CODE_CAMERA = 1234;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imgPicture = (ImageView) findViewById(R.id.imageViewPicture);
+        btnCamera = (Button) findViewById(R.id.buttonCamera);
+
+        btnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CAMERA}, REQUEST_CODE_CAMERA);
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(requestCode == REQUEST_CODE_CAMERA && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, REQUEST_CODE_CAMERA);
+        } else {
+            Toast.makeText(this, "Please allow the camera to open", Toast.LENGTH_SHORT).show();
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(requestCode == REQUEST_CODE_CAMERA && resultCode == RESULT_OK && data != null) {
+            Bitmap picture = (Bitmap) data.getExtras().get("data");     //data is default
+            imgPicture.setImageBitmap(picture);
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
