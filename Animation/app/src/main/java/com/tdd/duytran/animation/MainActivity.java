df0d741e221a1f6a17a4a5aa2267164757011b27
Duy Tran <tranducduy7520@gmail.com>
2020-09-30 21:41:44 +0700

    add Animation

---
+++ Animation/app/src/main/java/com/tdd/duytran/animation/MainActivity.java
@@ -0,0 +1,34 @@
+package com.tdd.duytran.animation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imgBell;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imgBell = (ImageView) findViewById(R.id.imageViewBell);
+
+        final Animation animAlpha = AnimationUtils.loadAnimation(this, R.anim.anim_alpha);
+        final Animation animRotate = AnimationUtils.loadAnimation(this, R.anim.anim_rotate);
+        final Animation animScale = AnimationUtils.loadAnimation(this, R.anim.anim_scale);
+        final Animation animTranslate = AnimationUtils.loadAnimation(this, R.anim.anim_translate);
+
+        imgBell.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                v.startAnimation(animTranslate);
+            }
+        });
+    }
+}
